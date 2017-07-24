27337b9d6e20bc4bb90cecf567e0ac575f8faa95
Ilene Gorski <ilene.gorski@gmail.com>
2017-07-24 13:50:57 -0700

    fix search from #show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/search/' => 'search#index'
+  get '/search/' => 'search#index', as: 'search'
   resources :projects, only: [:index]
   resources :clients do
     resources :projects, except: [:index]
