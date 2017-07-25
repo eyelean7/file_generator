a5535c6f1b7f17c2b7fcb0e65af070d5628c4465
Ilene Gorski <ilene.gorski@gmail.com>
2017-07-25 11:26:51 -0700

    allow user to create and delete clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,4 +6,26 @@ class ClientsController < ApplicationController
     @client = Client.find(params[:id])
     @projects = @client.projects
   end
+  def new
+    @client = Client.new()
+  end
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      redirect_to client_path(@client)
+    else
+      render :new
+    end
+  end
+  def destroy
+    @client = Client.find(params[:id])
+    @client.projects.destroy_all
+    @client.destroy
+    redirect_to clients_path
+  end
+
+  private
+  def client_params
+    params.require(:client).permit(:name, :billing_address, :phone)
+  end
 end
